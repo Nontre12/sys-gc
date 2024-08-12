3f8868b06dbdacc1e6a0bf59ba5acbf83a122afe
Nontre12 <62849884+Nontre12@users.noreply.github.com>
2024-08-12 21:57:14 +0200

    Updated example GuiTest class for printing overlay name and version

--- source/gui.cpp
+++ source/gui.cpp
@@ -3,21 +3,12 @@
 GuiTest::GuiTest() {}
 
 tsl::elm::Element *GuiTest::createUI() {
-  // A OverlayFrame is the base element every overlay consists of. This will draw the default
-  // Title and Subtitle. If you need more information in the header or want to change it's look,
-  // use a HeaderOverlayFrame.
-  auto frame = new tsl::elm::OverlayFrame("Tesla Example", "v1.3.1");
+  auto frame = new tsl::elm::OverlayFrame("sys-gc", "v0.0.1");
 
-  // A list that can contain sub elements and handles scrolling
   auto list = new tsl::elm::List();
-
-  // Create and add a new list item to the list
   list->addItem(new tsl::elm::ListItem("Default List Item"));
-
-  // Add the list to the frame for it to be drawn
   frame->setContent(list);
 
-  // Return the frame to have it become the top level element of this Gui
   return frame;
 }
 
